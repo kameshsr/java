44f9020510cb5bb12c63dc7b3789b79320d61310
kameshsr <kameshsr1338@gmail.com>
2025-07-14 21:05:06 +0530

    Added Program for valid

--- mystring/src/SubStringCons.java
+++ mystring/src/SubStringCons.java
@@ -5,5 +5,19 @@ public class SubStringCons{
         System.out.println(s);
         String s1=new String(ascii,1,2);
         System.out.println(s1);
+
+        StringBuffer sb = new StringBuffer("mallu");
+        System.out.println("buffer before "+sb);
+        System.out.println("charAt(1) before "+sb.charAt(1));
+
+        sb.setCharAt(1,'k');
+        sb.setLength(2);
+        System.out.println("buffer after "+sb);
+        System.out.println("charAt(1) after "+sb.charAt(1));
+
+        StringBuffer sb1 = new StringBuffer(40);
+        int i=40;
+        String s2= sb1.append("a! ").append(i).toString();
+        System.out.println(s2);
     }
 }
