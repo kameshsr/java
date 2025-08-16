e3c49ad6ce740ffb9b5f2e128f538ab5b120dc52
kameshsr <kameshsr1338@gmail.com>
2025-08-16 23:03:21 +0530

    Added oops program

---
+++ mystring/src/FlatMap.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FlatMap {
+    public static void main(String[] args){
+        List<List<Integer>> list = new ArrayList<>();
+        list.add(Arrays.asList(3,4));
+        list.add(Arrays.asList(1,6));
+        List<Integer> flat=list.stream().flatMap(list1-> list1.stream()).collect(Collectors.toList());
+        System.out.println(flat);
+    }
+    
+}
